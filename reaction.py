8b85f6e6bdd8e4213eca78e43dc63c3ee30f122b
f1ber1 <976559217a@gmail.com>
2025-04-11 03:02:24 -0900

    renew the button to the name

--- reaction.py
+++ reaction.py
@@ -1,17 +1,28 @@
 from gpiozero import LED,Button
 from time import sleep
 from random import uniform
+from signal import pause
 
 led = LED(4)
 right_button = Button(15)
 left_button = Button(14)
 
+
+left_name = input('left player name is:')
+right_name = input('right player name is:')
+
 def pressed(button):
-	print(str(button.pin.number) + 'won the game')
+	if button.pin.number == 14:
+		print(left_name +'won the game')
+		exit()
+	else:
+		print(right_name +'won the game')
+		exit()
 
 led.on()
 sleep(uniform(5,10))
 led.off()
-
 right_button.when_pressed=pressed
 left_button.when_pressed=pressed
+
+
