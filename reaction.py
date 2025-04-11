d8064fa1f90bd3bab89c1959a0959f2a7a088f17
f1ber1 <976559217a@gmail.com>
2025-04-11 03:23:31 -0900

    add the circle to the reaction.py

--- reaction.py
+++ reaction.py
@@ -11,18 +11,32 @@ left_button = Button(14)
 left_name = input('left player name is:')
 right_name = input('right player name is:')
 
-def pressed(button):
-	if button.pin.number == 14:
-		print(left_name +'won the game')
-		exit()
-	else:
-		print(right_name +'won the game')
-		exit()
+game_active = False
 
-led.on()
-sleep(uniform(5,10))
-led.off()
-right_button.when_pressed=pressed
-left_button.when_pressed=pressed
+def pressed(button):
+	global game_active
+	if game_active:
+		winner = left_name if button.pin.number == 14 else right_name
+		print(f"\n{winner} won the game!")
+		game_active = False
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
+
+try:
+	while True:
+		game_active = False
+		print("New round")
+		led.on()
+		delay = uniform(5,10)
+		sleep(delay)
+		led.off()
+		game_active = True
+		timeout = 5
+		while timeout > 0 and game_active:
+			sleep(0.1)
+			timeout -=0.1
+except KeyboardInterrupt:
+	led.close()
+	print("game over")
 
 
